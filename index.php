e081365f576a3c2c7a2db577703f85fee43da436
ingevers4 <ingevers4@student.arteveldehs.be>
2017-10-05 09:38:38 +0200

    WIP

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$naam = 'Inge';
+$hallo = "hallo ";
+$wereld = "wereld";
+$x = 10;
+$y = 7;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $naam ?> <br>
+    <?php echo $hallo , $wereld ?> <br>
+    <?php echo "7 + 10 is ", $y + $x ?> <br>
+    <?php echo "7 - 10 is ", $y - $x ?> <br>
+    <?php echo "7 * 10 is ", $y * $x ?> <br>
+    <?php echo "7 / 10 is ", $y / $x ?> <br>
+    <?php echo "7 % 10 is ", $y % $x ?> <br>
+
+
+    <h1>Titel in HTML </h1>
+</body>
+</html>
